6c436e618f005f6916774e89d884afd9a766d576
Rahul Kumar <rahulchaudhary07082004@gmail.com>
2026-01-10 23:51:39 +0530

    Things i know today 1.Create custom react 2.Create custom render method 3.Learn about jsx 4.Learn about creating Components 5.Learn about evaluated expression (any JavaScript expression that is executed and resolved to a value inside JSX) and how to eject it 6.Go through the github repo of react to know more about it 7.Know about how react make tree by taking my html code 8.Create react element and know about React.createElement and its work

--- reactwithvite/src/main.jsx
+++ reactwithvite/src/main.jsx
@@ -5,17 +5,34 @@ import App from "./App.jsx";
 import React from "react";
 
 function MyApp() {
-    return (
-        <div>
-            <h1>custom app | chai </h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>custom app | chai </h1>
+    </div>
+  );
 }
+/* const reactElement = {  The code of this section will not get rendered because 
+    type: 'a',             this is my custom react and for this i have custom renderer.               
+    props: {               
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}*/
 
 const anotherElement = (
-    <a href="https://google.com" target="_blank">visit google</a>
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Click me to visit google',
+    anotherUser
 )
 
-createRoot(document.getElementById("root")).render(
-    anotherElement
-);
+createRoot(document.getElementById("root")).render(reactElement);
